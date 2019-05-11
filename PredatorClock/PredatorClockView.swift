4efd7108a1552425e1b2acaf34ca4adbe27f8088
Victor Peschenkov <v.peschenkov@gmail.com>
2019-05-11 14:40:40 +0300

    Fix bugs

--- PredatorClock/PredatorClockView.swift
+++ PredatorClock/PredatorClockView.swift
@@ -9,22 +9,47 @@
 import ScreenSaver
 
 class PredatorClockView: ScreenSaverView {
+    private var timer: Timer?
     private let predator = PredatorClock()
+    
+    override init?(frame: NSRect, isPreview: Bool) {
+        super.init(frame: frame, isPreview: isPreview)
+        animationTimeInterval = 1 / 30
+        startAnimation()
+    }
+    
+    required init?(coder decoder: NSCoder) {
+        super.init(coder: decoder)
+        animationTimeInterval = 1 / 30
+        startAnimation()
+    }
     
     override var isAnimating: Bool {
         return true
     }
     
     override var hasConfigureSheet: Bool {
-        return true
+        return false
     }
     
     override func draw(_ rect: NSRect) {
         super.draw(rect)
-        
         if let context = NSGraphicsContext.current?.cgContext {
-            predator.draw(in: rect, context: context)
+            predator.draw(in: self.visibleRect, context: context)
         }
+    }
+    
+    override func startAnimation() {
+        super.startAnimation()
+        timer = Timer.scheduledTimer(withTimeInterval: animationTimeInterval, repeats: true) { t in
+            self.setNeedsDisplay(self.bounds)
+        }
+    }
+    
+    override func stopAnimation() {
+        super.stopAnimation()
+        timer?.invalidate()
+        timer = nil
     }
     
     override var configureSheet: NSWindow? {
--- PredatorClockCore/PredatorClockSettings.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PredatorClockSettings.swift
-//  PredatorClockCore
-//
-//  Created by Victor Peschenkov on 5/10/19.
-//  Copyright © 2019 Victor Peschenkov. All rights reserved.
-//
-
-import Foundation
